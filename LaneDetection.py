98baa40a7c6bdb7416fdd88bde9b776364566706
Tyler Le <tylerle99@yahoo.com>
2020-12-07 20:08:50 -0800

    Added video stack for display and fixed division by 0 bug when slope = 0.

--- LaneDetection.py
+++ LaneDetection.py
@@ -3,6 +3,40 @@ import cv2
 import matplotlib.pyplot as plt
 
 
+# Function to stack videos for cleaner display output.
+def stackVideos(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
 def toCanny(image):
     # Convert originalImage to grayscale
     originalToGray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -39,26 +73,28 @@ def makeCoordinates(image, lineParameters):
 
 
 def averageSlopeIntercept(image, lines):
+    global leftLine, rightLine
     left = []  # Has negative slope
     right = []  # Has positive slope
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            parameters = np.polyfit((x1, x2), (y1, y2), 1)
-            slope = parameters[0]
-            intercept = parameters[1]
-            if slope < 0:
-                left.append((slope, intercept))
-            else:
-                right.append((slope, intercept))
+
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left.append((slope, intercept))
+        else:
+            right.append((slope, intercept))
 
     # Average out slope and y-intercept in each of the left and right lists
-    leftAverage = np.average(left, axis=0)
-    rightAverage = np.average(right, axis=0)
-
     # Create lines from these averaged values
-    leftLine = makeCoordinates(image, leftAverage)
-    rightLine = makeCoordinates(image, rightAverage)
+    if left:
+        leftAverage = np.average(left, axis=0)
+        leftLine = makeCoordinates(image, leftAverage)
+    if right:
+        rightAverage = np.average(right, axis=0)
+        rightLine = makeCoordinates(image, rightAverage)
 
     return np.array([leftLine, rightLine])
 
@@ -97,7 +133,8 @@ while cap.isOpened():
     # Combine lineImage and colorImage
     combinedImages = cv2.addWeighted(frame, 0.8, lineImage, 1, 1)
 
-    cv2.imshow("results", combinedImages)
+    vidStack = stackVideos(0.5, ([cannyImage, croppedImage],[lineImage, combinedImages]))
+    cv2.imshow("Videos", vidStack)
 
     if cv2.waitKey(1) == ord('q'):
         break
